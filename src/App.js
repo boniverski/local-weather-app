4460b0dceb6ad1b6dd97be2c2bc62a0970f41356
boniverski <bosko.rabrenovic@gmail.com>
2017-11-16 11:02:42 +0100

    fetch time & icons

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 //Setting up a promise for user's location
 const getLocation = () => {
-  const geolocation = navigator.geolocation;
 
+  const geolocation = navigator.geolocation;
   const location = new Promise((resolve, reject) => {
     if (!geolocation) {
       reject(new Error('Geolocation is not supported by this browser!'));
@@ -20,6 +20,17 @@ const getLocation = () => {
   return location;
 };
 
+//Converts Unix timestamp
+const getTime = (data) => {
+
+  const date = new Date(data.dt * 1000);
+  const hours = (date.getHours().toString().length === 1) ? ('0' + date.getHours()) : (date.getMinutes());
+  const minutes = (date.getMinutes().toString().length === 1) ? ('0' + date.getMinutes()) : (date.getMinutes());
+  const formattedTime = hours + ':' + minutes;
+
+  return formattedTime;
+}
+
 class App extends Component {
 
   constructor(props){
@@ -29,7 +40,11 @@ class App extends Component {
       latitude: null,
       longitude: null,
       error: '',
-      description: ''
+      description: '',
+      time: '',
+      sunrise: '',
+      sunset: '',
+      icon: ''
     }
   }
 
@@ -49,15 +64,25 @@ class App extends Component {
     });
   }
 
-  //Callback function - Fetching weather data with Open Weather Api
+  //Callback function for getLocation() - Fetching weather data with Open Weather Api
   getWeather() {
-    fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${(this.state.latitude)}&lon=${(this.state.longitude)}&APPID=5a5a02f356f4f64fe223c5d5a5efde42`)
+    fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&APPID=5a5a02f356f4f64fe223c5d5a5efde42`)
     .then(data => data.json())
     .then((response) => {
       console.log(response);
+      const formattedTime = getTime(response);
+      const currentTime = response.dt;
+      const sunrise = response.sys.sunrise;
+      const sunset = response.sys.sunset;
+
       this.setState({
         description: response.weather[0].description,
+        time: formattedTime,
+        sunrise: response.sys.sunrise,
+        sunset: response.sys.sunset,
+        icon: (currentTime > sunrise && currentTime < sunset) ? ('wi wi-owm-day-' + response.weather[0].id + '') : ('wi wi-owm-night-' + response.weather[0].id + '')
       })
+
     })
     .catch((e) => {
       this.setState({
@@ -74,13 +99,22 @@ class App extends Component {
       <div className="wrapper">
         <div className="app">
           { !hasError ?
-            (<h2>location: {this.state.latitude} {this.state.longitude} {this.state.description}</h2>)
+            <div>
+              <h2>location: {this.state.time} {this.state.latitude} {this.state.longitude} {this.state.description}</h2>
+              <i className={this.state.icon}></i>
 
+            </div>
             : (hasError)}
         </div>
       </div>
     )
   }
 }
+
+// const Icon = (props) => {
+//   return(
+//     <i className={props}></i>
+//   )
+// }
 
 export default App;
